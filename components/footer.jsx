6c8b05154fed069efe82ac5a5003d389a943fae4
Rohan Godha <rgodha24@brophybroncos.org>
2024-07-10 17:11:13 -0700

    change footer + package manager

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
-    return (
-      <footer className="w-full py-4 text-black dark:text-white text-center">
-        <p>Made with ❤️ by <a href="https://about.shangen.org">Shang En</a></p>
-        <p><a href="https://github.com/12458/gthousing">Source Code</a></p>
-      </footer>
-    );
-  };
-  
+  return (
+    <footer className="w-full py-4 text-black dark:text-white text-center">
+      <p>Made with ❤️ by <a href="https://about.shangen.org">Shang En</a> and modified with ❤️  by <a href="https://rohangodha.com">Rohan Godha</a></p>
+      <p><a href="https://github.com/rgodha24/gthousing">Source Code</a></p>
+    </footer>
+  );
+};
+
 export default Footer;
